42a7f27d0e7c75260126c5df1ee7bfa0cd706df0
JingYu Ning <foldfelis@gmail.com>
2021-03-12 21:50:04 +0800

    fix test bug

--- test/logistic_regression.jl
+++ test/logistic_regression.jl
@@ -2,7 +2,7 @@
     @test isapprox(ML101.sigmoid(0), 0.5, atol=1e-10)
 
     lrm = LogisticRegressionModel(
-        vcat(randn(10).+3, randn(10).+30),
+        vcat(ones(10), zeros(10)),
         hcat(
             vcat(randn(10).+1, randn(10).+10),
             vcat(randn(10).+2, randn(10).+20)
@@ -17,7 +17,7 @@ end
     df = DataFrame(
         X₁=vcat(randn(10).+1, randn(10).+10),
         X₂=vcat(randn(10).+2, randn(10).+20),
-        Y=vcat(randn(10).+3, randn(10).+30)
+        Y=vcat(ones(10), zeros(10))
     )
     lrm = LogisticRegressionModel(df, :Y, [:X₁, :X₂])
     lrm.argv = [3.0, 5.0]
@@ -27,11 +27,11 @@ end
     @test isapprox(
         ML101.log_likelyhood(lrm),
         (
-            3 * log(ML101.sigmoid([3.0, 5.0]' * [1, 2])) +
-            (1-3) * log(1 - ML101.sigmoid([3.0, 5.0]' * [1, 2]))
+            1 * log(ML101.sigmoid([3.0, 5.0]' * [1, 2])) +
+            (1-1) * log(1 - ML101.sigmoid([3.0, 5.0]' * [1, 2]))
         ) + (
-            30 * log(ML101.sigmoid([3.0, 5.0]' * [10, 20])) +
-            (1-30) * log(1 - ML101.sigmoid([3.0, 5.0]' * [10, 20]))
+            0 * log(ML101.sigmoid([3.0, 5.0]' * [10, 20])) +
+            (1-0) * log(1 - ML101.sigmoid([3.0, 5.0]' * [10, 20]))
         ),
         atol=1e-10
     )
