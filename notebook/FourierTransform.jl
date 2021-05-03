bf5a382c388c6c8128630fc948173fc63e77d4c9
JingYu Ning <foldfelis@gmail.com>
2021-05-03 17:03:21 +0800

    reconstruct signal and plot

--- notebook/FourierTransform.jl
+++ notebook/FourierTransform.jl
@@ -1,8 +1,16 @@
 ### A Pluto.jl notebook ###
-# v0.14.4
+# v0.14.5
 
 using Markdown
 using InteractiveUtils
+
+# ╔═╡ 9fa2be99-1c4a-4e53-8a01-546a3670f19f
+begin
+	using FFTW
+	using Flux
+	using Plots
+	plotly()
+end
 
 # ╔═╡ b682a76e-75c6-4e0f-b542-210725501e5b
 html"""<style>
@@ -67,33 +75,87 @@ function create_𝐰_fourier(len::Integer)
 	end
 	
 	return 𝐰
-end
+end;
+
+# ╔═╡ 45844ffc-9f5c-4952-abde-4ce770ab77f6
+function create_𝐰¯¹_fourier(len::Integer)
+	𝐰¯¹ = Matrix{ComplexF64}(undef, len, len)
+	for n in 0:len-1, k in 0:len-1
+		𝐰¯¹[n+1, k+1] = exp(im*2π*k*n/len)
+	end
+	
+	return 𝐰¯¹/len
+end;
+
+# ╔═╡ 212ac582-1270-49da-9cd4-078ab695be66
+md"
+Generate random signal
+
+$x = \begin{bmatrix} x_0\ ...\ x_{N-1} \end{bmatrix}$
+"
 
 # ╔═╡ 7efa004c-57c4-4b9c-bc74-90252de25218
 begin
 	len = 64
 	x = rand(len)'
-end
+end;
 
-# ╔═╡ 41f594d4-5747-4000-a6be-f23768c01460
-begin
-	using FFTW
-	y = fft(x)
-end
+# ╔═╡ 036016cd-e4f5-40f7-96d9-cac8fdbebd01
+md"
+Do the Fourier transform by Fourier seights and FFT
+
+$x̂ = Fx$
+"
 
 # ╔═╡ d5933fdb-b9ed-4475-af43-58dac24a47d5
 begin	
+	# Fourier transform via Fourier weights
 	𝐰 = create_𝐰_fourier(len)
-	ŷ = x * 𝐰
+	x̂ = x * 𝐰
+	
+	# Fourier transform via fft
+	x̂_fft = fft(x)
+
+	# root-mean-sqrt error between those two
+	ϵ = sqrt(sum(abs.(x̂_fft - x̂).^2)/len)
+end;
+
+# ╔═╡ ac36e1c5-dade-4b0f-974e-2d261ea260f8
+begin
+	# reconstruct signal via Fourier weights
+	𝐰¯¹ = create_𝐰¯¹_fourier(len)
+	𝑓¯¹x̂ = real(x̂ * 𝐰¯¹)'
+	
+	# reconstruct signal via ifft
+	𝑓¯¹x̂_fft = real(ifft(x̂_fft))'
+end;
+
+# ╔═╡ e9c246d8-ae77-4236-8ca0-15de7cced221
+begin
+	plot(title="Signal", xlabel="Time (arb. unit)", ylabel="Amp (arb. unit)", ticks=[], ylims=(-2.5, 2))
+	plot!(x', label="Original signal")
+	plot!(𝑓¯¹x̂ .- 1, label="Reconstructed signal (Fourier weights)")
+	plot!(𝑓¯¹x̂_fft .- 2, label="Reconstructed signal (ifft)")
 end
+
+# ╔═╡ 3c03ec6d-8994-4886-9006-3cddbf0ac027
+md"
+## Learning the Fourier transform via gradient-descent
+"
 
 # ╔═╡ Cell order:
 # ╟─b682a76e-75c6-4e0f-b542-210725501e5b
 # ╟─b5ba5d3a-eb07-4d5f-a802-6d85075ed528
+# ╠═9fa2be99-1c4a-4e53-8a01-546a3670f19f
 # ╟─21b015ea-aa4b-11eb-182b-a7590312f5ba
 # ╟─5eeee95e-ce21-4681-a834-9b8e30b935e8
 # ╟─0d6607c2-0881-40f7-81fd-494d49faa697
 # ╠═1f66b59e-bdcd-4cdb-9f28-3bbf0f7dd08e
+# ╠═45844ffc-9f5c-4952-abde-4ce770ab77f6
+# ╟─212ac582-1270-49da-9cd4-078ab695be66
 # ╠═7efa004c-57c4-4b9c-bc74-90252de25218
+# ╟─036016cd-e4f5-40f7-96d9-cac8fdbebd01
 # ╠═d5933fdb-b9ed-4475-af43-58dac24a47d5
-# ╠═41f594d4-5747-4000-a6be-f23768c01460
+# ╠═ac36e1c5-dade-4b0f-974e-2d261ea260f8
+# ╠═e9c246d8-ae77-4236-8ca0-15de7cced221
+# ╟─3c03ec6d-8994-4886-9006-3cddbf0ac027
