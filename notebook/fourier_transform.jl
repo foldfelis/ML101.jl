35553a6f56166e777713fea30951d3f62e019c9e
JingYu Ning <foldfelis@gmail.com>
2021-05-04 12:17:23 +0800

    refactor

--- notebook/fourier_transform.jl
+++ notebook/fourier_transform.jl
@@ -62,6 +62,19 @@ $\begin{bmatrix} y_0\ ...\ y_{N-1} \end{bmatrix} =
 $y = xW_{ComplexFourier}$
 "
 
+# ╔═╡ 212ac582-1270-49da-9cd4-078ab695be66
+md"
+## Generate random signal
+
+$x = \begin{bmatrix} x_0\ ...\ x_{N-1} \end{bmatrix}$
+"
+
+# ╔═╡ 7efa004c-57c4-4b9c-bc74-90252de25218
+begin
+	len = 64
+	x = rand(len)'
+end;
+
 # ╔═╡ 0d6607c2-0881-40f7-81fd-494d49faa697
 md"
 ## Computing the Fourier transform using Fourier weights
@@ -77,32 +90,9 @@ function create_𝐰_fourier(len::Integer)
 	return 𝐰
 end;
 
-# ╔═╡ 45844ffc-9f5c-4952-abde-4ce770ab77f6
-function create_𝐰¯¹_fourier(len::Integer)
-	𝐰¯¹ = Matrix{ComplexF64}(undef, len, len)
-	for n in 0:len-1, k in 0:len-1
-		𝐰¯¹[n+1, k+1] = exp(im*2π*k*n/len)
-	end
-	
-	return 𝐰¯¹/len
-end;
-
-# ╔═╡ 212ac582-1270-49da-9cd4-078ab695be66
-md"
-Generate random signal
-
-$x = \begin{bmatrix} x_0\ ...\ x_{N-1} \end{bmatrix}$
-"
-
-# ╔═╡ 7efa004c-57c4-4b9c-bc74-90252de25218
-begin
-	len = 64
-	x = rand(len)'
-end;
-
 # ╔═╡ 036016cd-e4f5-40f7-96d9-cac8fdbebd01
 md"
-Do the Fourier transform by Fourier seights and FFT
+#### Do the Fourier transform by Fourier weights and FFT
 
 $x̂ = Fx$
 "
@@ -117,7 +107,34 @@ begin
 	x̂_fft = fft(x)
 
 	# root-mean-sqrt error between those two
-	ϵ = sqrt(sum(abs.(x̂_fft - x̂).^2)/len)
+	ϵ_x̂ = sqrt(sum(abs.(x̂_fft - x̂).^2)/len)
+end;
+
+# ╔═╡ 9d937bef-f325-4299-8c4b-7ae358aeb1c9
+md"
+RMSE: $ϵ_x̂
+"
+
+# ╔═╡ f65c7059-9c2e-4993-a7e6-32cb9a848f23
+begin
+	plot(title="Spectrum", xlabel="Freq (arb. unit)", ylabel="Amp (arb. unit)")
+	plot!(real(x̂_fft'), label="Fourier weights")
+	plot!(real(x̂'), label="FFT")
+end
+
+# ╔═╡ 6e200b44-97fb-43e0-8026-aabef11f4f17
+md"
+#### Reconstruct signal by inverse Fourier weights and iFFT
+"
+
+# ╔═╡ 45844ffc-9f5c-4952-abde-4ce770ab77f6
+function create_𝐰¯¹_fourier(len::Integer)
+	𝐰¯¹ = Matrix{ComplexF64}(undef, len, len)
+	for n in 0:len-1, k in 0:len-1
+		𝐰¯¹[n+1, k+1] = exp(im*2π*k*n/len)
+	end
+	
+	return 𝐰¯¹/len
 end;
 
 # ╔═╡ ac36e1c5-dade-4b0f-974e-2d261ea260f8
@@ -128,14 +145,26 @@ begin
 	
 	# reconstruct signal via ifft
 	𝑓¯¹x̂_fft = real(ifft(x̂_fft))'
+	
+	# root-mean-sqrt error between those two
+	ϵ_𝑓¯¹x̂_fft = sqrt(sum(abs.(𝑓¯¹x̂_fft - x').^2)/len)
+	ϵ_𝑓¯¹x̂ = sqrt(sum(abs.(𝑓¯¹x̂ - x').^2)/len)
 end;
+
+# ╔═╡ 7339df20-651e-42ac-8286-e9696fb72458
+md"
+RMSE: 
+
+* Via IFFT: $ϵ_𝑓¯¹x̂_fft
+* Via Fourier waights: $ϵ_𝑓¯¹x̂ 
+"
 
 # ╔═╡ e9c246d8-ae77-4236-8ca0-15de7cced221
 begin
-	plot(title="Signal", xlabel="Time (arb. unit)", ylabel="Amp (arb. unit)", ticks=[], ylims=(-2.5, 2))
-	plot!(x', label="Original signal")
-	plot!(𝑓¯¹x̂ .- 1, label="Reconstructed signal (Fourier weights)")
-	plot!(𝑓¯¹x̂_fft .- 2, label="Reconstructed signal (ifft)")
+	plot(title="Signal", xlabel="Time (arb. unit)", ylabel="Amp (arb. unit)", ticks=[], ylims=(0, 3.5))
+	plot!(x' .+2 , label="Original signal")
+	plot!(𝑓¯¹x̂ .+ 1, label="Reconstructed signal (Fourier weights)")
+	plot!(𝑓¯¹x̂_fft, label="Reconstructed signal (ifft)")
 end
 
 # ╔═╡ 3c03ec6d-8994-4886-9006-3cddbf0ac027
@@ -149,13 +178,17 @@ md"
 # ╠═9fa2be99-1c4a-4e53-8a01-546a3670f19f
 # ╟─21b015ea-aa4b-11eb-182b-a7590312f5ba
 # ╟─5eeee95e-ce21-4681-a834-9b8e30b935e8
-# ╟─0d6607c2-0881-40f7-81fd-494d49faa697
-# ╠═1f66b59e-bdcd-4cdb-9f28-3bbf0f7dd08e
-# ╠═45844ffc-9f5c-4952-abde-4ce770ab77f6
 # ╟─212ac582-1270-49da-9cd4-078ab695be66
 # ╠═7efa004c-57c4-4b9c-bc74-90252de25218
+# ╟─0d6607c2-0881-40f7-81fd-494d49faa697
+# ╠═1f66b59e-bdcd-4cdb-9f28-3bbf0f7dd08e
 # ╟─036016cd-e4f5-40f7-96d9-cac8fdbebd01
 # ╠═d5933fdb-b9ed-4475-af43-58dac24a47d5
+# ╟─9d937bef-f325-4299-8c4b-7ae358aeb1c9
+# ╠═f65c7059-9c2e-4993-a7e6-32cb9a848f23
+# ╟─6e200b44-97fb-43e0-8026-aabef11f4f17
+# ╠═45844ffc-9f5c-4952-abde-4ce770ab77f6
 # ╠═ac36e1c5-dade-4b0f-974e-2d261ea260f8
+# ╟─7339df20-651e-42ac-8286-e9696fb72458
 # ╠═e9c246d8-ae77-4236-8ca0-15de7cced221
 # ╟─3c03ec6d-8994-4886-9006-3cddbf0ac027
