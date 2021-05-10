ae05274fdafbc682857e92146f06b70a2a340920
JingYu Ning <foldfelis@gmail.com>
2021-05-11 01:18:00 +0800

    irls

---
+++ notebook/irls.jl
@@ -0,0 +1,43 @@
+### A Pluto.jl notebook ###
+# v0.14.5
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 947fbdc9-3c48-44f3-b4d3-183f70e2194e
+using LinearAlgebra
+
+# ╔═╡ a465636e-bc12-4e8f-9db2-1a20ea771504
+begin
+	𝐱 = rand(100, 3)
+	𝐱 = hcat(ones(size(𝐱, 1)), 𝐱)
+	y = 𝐱 * [1, 2, 3, 4] + 1e-3randn(size(𝐱, 1))
+end;
+
+# ╔═╡ 833c56c2-d2ab-4f94-968b-b5771c9fa945
+function irls(𝐱, y; p=2, n_iter=100)
+	w = ones(size(𝐱, 1))
+	𝐰 = diagm(w)
+	β = inv(𝐱' * 𝐰 * 𝐱) * 𝐱' * 𝐰 * y
+	
+	if p == 2
+		return β
+	end
+	
+	for _ in 1:n_iter
+		w = abs.(y - 𝐱 * β).^(p-2)
+		𝐰 = diagm(w)
+		β = inv(𝐱' * 𝐰 * 𝐱) * 𝐱' * 𝐰 * y
+	end
+	
+	return β
+end
+
+# ╔═╡ 2c663777-b6e3-42b2-a279-d7b42dd27d70
+irls(𝐱, y, p=4, n_iter=10)
+
+# ╔═╡ Cell order:
+# ╠═947fbdc9-3c48-44f3-b4d3-183f70e2194e
+# ╠═a465636e-bc12-4e8f-9db2-1a20ea771504
+# ╠═833c56c2-d2ab-4f94-968b-b5771c9fa945
+# ╠═2c663777-b6e3-42b2-a279-d7b42dd27d70
