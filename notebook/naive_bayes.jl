17b9c3249b519b2f72dbc01686331c7ef0c1eb85
JingYu Ning <foldfelis@gmail.com>
2021-07-22 03:32:21 +0800

    refactor

--- notebook/naive_bayes.jl
+++ notebook/naive_bayes.jl
@@ -22,8 +22,13 @@ sms2token(sms::String) = [m.match for m in eachmatch(r"[a-z']+", lowercase(sms))
 # ╔═╡ a7b1da07-fa68-44c9-b01d-b6b0196f02b1
 begin
 	df_raw = CSV.read("../data/SMSSpamCollection", DataFrame, header=[:Label, :SMS])
+	
 	df = copy(df_raw) 
 	df[!, :SMS] .= sms2token.(df.SMS)
+	
+	spam_df = df[df.Label.=="spam", :]
+	ham_df = df[df.Label.=="ham", :]
+	
 	df_raw
 end
 
@@ -32,47 +37,45 @@ md"
 ## Text mining
 "
 
-# ╔═╡ dada49f4-63e1-43cc-8035-158852d6db8c
-function calc_freq(sms::Vector)
-	freq = Dict([(i, Float64(count(x->x==i, sms))) for i in unique(sms)])
-	total_count = sum(values(freq))
-	for (k, _) in freq
-		freq[k] /= total_count
-	end
+# ╔═╡ b4a30d03-5f3c-4fb3-9351-9c8f689032a2
+begin
+	features = unique(vcat(df.SMS...))
 	
-	return freq
+	function calc_tf(words::Vector)
+		tf = Dict([(w, Float64(count(x->x==w, words))) for w in features])
+		
+		n = sum(values(tf))
+		for (k, v) in tf
+			tf[k] = (v == 0) ? 1e-1/n : v/n
+		end
+		
+		return tf
+	end
 end
 
-# ╔═╡ 112ca24c-2f1b-41ce-9645-2d5f2a1297ff
-function calc_idf(sms::Vector) 
-	idf = calc_freq(sms)
-	for (k, v) in idf
-		idf[k] = log(1/v)
-	end
+# ╔═╡ f10b2755-70d1-4cfe-a759-17f283bb7cb3
+begin
+	spam_p = nrow(spam_df) / nrow(df)
+	spam_tf = calc_tf(vcat(spam_df.SMS...))
 	
-	return idf
-end
+	ham_p = nrow(ham_df) / nrow(df)
+	ham_tf = calc_tf(vcat(ham_df.SMS...))
+end;
 
 # ╔═╡ ac757e08-0b19-4aa8-a791-8a677280b12f
-function plot_word_cloud(idf::Dict, top_n::Integer)
+function plot_word_cloud(tf::Dict, top_n::Integer)
+	tf = sort(collect(tf), by=x->x[2])
+	
 	return wordcloud(
-		[keys(idf)...][1:top_n], 
-		[values(idf)...][1:top_n],
+		map(x->x[1], tf)[1:top_n], 
+		map(x->x[2], tf)[1:top_n],
 		font="LiSong Pro",
 		mask=shape(box, 400*2, 300*2, 10*2),
 		colors=:Dark2_3,
 		angles=(0, 90),
-		density=0.3
+		density=0.4
 	)
 end
-
-# ╔═╡ 0b173585-640f-4687-a42a-980142fd7aea
-begin
-	hem_sms = vcat(df[df.Label.=="ham", :SMS]...)
-	hem_idf = calc_idf(hem_sms)
-	spam_sms = vcat(df[df.Label.=="spam", :SMS]...)
-	spam_idf = calc_idf(spam_sms)
-end;
 
 # ╔═╡ 7f30886f-32e3-498c-8166-ea5f18b5e725
 md"
@@ -80,7 +83,7 @@ md"
 "
 
 # ╔═╡ d9127fa7-e83d-4c21-a2d0-9fa497e1af44
-plot_word_cloud(hem_idf, 200)
+plot_word_cloud(spam_tf, 400)
 
 # ╔═╡ 1fca2c23-5ff9-42cc-a5fa-a872d4cf0cab
 md"
@@ -88,7 +91,7 @@ md"
 "
 
 # ╔═╡ b4d6a0ed-8e41-44d1-b736-e3acc1cd511e
-plot_word_cloud(spam_idf, 200)
+plot_word_cloud(ham_tf, 400)
 
 # ╔═╡ e7997329-45fa-42cb-8d6b-399089099110
 md"
@@ -103,30 +106,6 @@ $P(Cs|\vec{x}) = \frac{P(\vec{x}|Cs) P(Cs)}{P(\vec{x}|Cs)P(Cs) + P(\vec{x}|Ch)P(
 
 $P(\vec{x}|C) = P(w1|C)P(w2|C)P(w3|C)...$
 "
-
-# ╔═╡ f10b2755-70d1-4cfe-a759-17f283bb7cb3
-begin
-	features = unique(vcat(df.SMS...))
-	
-	function calc_tf(words::Vector)
-		tf = Dict([(w, Float64(count(x->x==w, words))) for w in features])
-		
-		n = sum(values(tf))
-		for (k, v) in tf
-			tf[k] = (v == 0) ? 1e-1/n : v/n
-		end
-		
-		return tf
-	end
-	
-	spam_df = df[df.Label.=="spam", :]
-	spam_p = nrow(spam_df) / nrow(df)
-	spam_tf = calc_tf(vcat(spam_df.SMS...))
-	
-	ham_df = df[df.Label.=="ham", :]
-	ham_p = nrow(ham_df) / nrow(df)
-	ham_tf = calc_tf(vcat(ham_df.SMS...))
-end;
 
 # ╔═╡ a3f53b05-f2dd-46af-b573-3c5de2c3f709
 begin
@@ -1011,17 +990,15 @@ version = "3.5.0+0"
 # ╠═c9d86708-8ce9-48dd-8d18-ba04b4c4a22d
 # ╠═a7b1da07-fa68-44c9-b01d-b6b0196f02b1
 # ╟─6d356343-6f13-4792-b084-b55aa0af7510
-# ╠═dada49f4-63e1-43cc-8035-158852d6db8c
-# ╠═112ca24c-2f1b-41ce-9645-2d5f2a1297ff
+# ╠═b4a30d03-5f3c-4fb3-9351-9c8f689032a2
+# ╠═f10b2755-70d1-4cfe-a759-17f283bb7cb3
 # ╠═ac757e08-0b19-4aa8-a791-8a677280b12f
-# ╠═0b173585-640f-4687-a42a-980142fd7aea
 # ╟─7f30886f-32e3-498c-8166-ea5f18b5e725
 # ╠═d9127fa7-e83d-4c21-a2d0-9fa497e1af44
 # ╟─1fca2c23-5ff9-42cc-a5fa-a872d4cf0cab
 # ╠═b4d6a0ed-8e41-44d1-b736-e3acc1cd511e
 # ╟─e7997329-45fa-42cb-8d6b-399089099110
 # ╟─dcb7da4c-5092-4667-845a-af4d3cd7c9d3
-# ╠═f10b2755-70d1-4cfe-a759-17f283bb7cb3
 # ╠═a3f53b05-f2dd-46af-b573-3c5de2c3f709
 # ╟─f95f4e29-44ec-48ae-8ef3-878c3f88ba4c
 # ╠═60b39235-a767-452e-998a-6f6efa0a7c1e
