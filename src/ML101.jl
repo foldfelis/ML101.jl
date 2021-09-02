c4baa118188035654cdae79432b7c07a91031b8d
JingYu Ning <foldfelis@gmail.com>
2021-09-02 16:04:58 +0800

    flatten proj and implement PCA

--- src/ML101.jl
+++ src/ML101.jl
@@ -1,9 +1,2 @@
 module ML101
-
-    using DataFrames
-
-    include("linear_regression.jl")
-    include("logistic_regression.jl")
-    include("naive_bayes.jl")
-
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,6 @@
 using ML101
 using Test
 
-using DataFrames
-
 @testset "ML101.jl" begin
-
-    include("linear_regression.jl")
-    include("logistic_regression.jl")
 
 end
